1f842f92482da6c341d3079646539afd63f6c650
Hunter Yeago <hyradar@tutamail.com>
2022-09-04 09:41:58 +1000

    created basic layout and added grid and added basic hover functionality to grid

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+
+for (let i = 0; i < 16 * 16; i ++) {
+
+    let div = document.createElement('div');
+    div.className = "item";
+    
+    const mygrid = document.querySelector(".gamegrid");
+    mygrid.appendChild(div);
+
+
+}
