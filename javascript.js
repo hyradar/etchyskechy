c2d4af3f317ed9103f780ae9f945525838bbf4db
Hunter Yeago <hyradar@tutamail.com>
2022-09-18 09:35:58 +1000

    added rainbow functionality and switch between brushes

--- javascript.js
+++ javascript.js
@@ -1,8 +1,22 @@
 
-updateGrid();
 
-//Adds Event Listener to Slider Element
+
+updateGrid();
+let defaultBrush = "blueBrush";
+let globalBrush = defaultBrush;
+
+globalBrush = updateGlobalBrush(globalBrush);
+
+
+globalBrush = updateGlobalBrush("rainwbowBrush");
+
+
+
+//Adds Event Listeners
 document.querySelector('.slider').addEventListener('input', updateGrid);
+
+
+//Grid Functions
 
 function updateGrid() {
     let output = document.querySelector('.gridrange');
@@ -19,7 +33,7 @@ function removeGrid() {
     }
 }
 
-function updateBlock(grid) {
+function updateBlock(grid, globalBrush) {
     //Create Grid
     for (let i = 0; i < grid * grid; i ++) {
         
@@ -36,13 +50,56 @@ function updateBlock(grid) {
     addMouseOver();
 }
 
-function changeColor(event) {
+// Brush + Mouse Functions
+
+function blueBrush(event) {
     event.target.style.backgroundColor = "blue";
+}
+
+function rainwbowBrush(event) {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    event.target.style.backgroundColor = color;
 }
 
 function addMouseOver() {
     const nodeList = document.querySelectorAll(".blocks");
     for (let i = 0; i < nodeList.length; i ++) {
-        nodeList[i].addEventListener("mouseover" , changeColor);
+        nodeList[i].addEventListener("mouseover" , blueBrush);
     }
+}
+
+
+function updateGlobalBrush(brush) {
+    
+    const nodeList = document.querySelectorAll(".blocks");
+    
+    if (brush === "blueBrush") {
+        //Remove Event Listeners
+        for (let i = 0; i < nodeList.length; i ++) {
+            nodeList[i].removeEventListener("mouseover" , rainwbowBrush);
+        }
+        //Add Event Listeners
+        for (let i = 0; i < nodeList.length; i ++) {
+            nodeList[i].addEventListener("mouseover" , blueBrush);
+        }
+        return brush = "blueBrush";
+    }
+    
+    else if (brush === "rainbowBrush") {
+        //Remove Event Listeners
+        for (let i = 0; i < nodeList.length; i ++) {
+            nodeList[i].removeEventListener("mouseover" , blueBrush);
+        }
+        //Add Event Listeners
+        for (let i = 0; i < nodeList.length; i ++) {
+            nodeList[i].addEventListener("mouseover" , rainwbowBrush);
+        }
+        return brush = "rainbowBrush";
+    }
+    return brush;
 }
