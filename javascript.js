5e8498ed10214842b456cd485f37ce73616ddd9a
Hunter Yeago <hyradar@tutamail.com>
2022-09-17 20:19:59 +1000

    added hover to make squares blue functionality

--- javascript.js
+++ javascript.js
@@ -1,12 +1,23 @@
 
 
+
+//Create the Grid
 for (let i = 0; i < 16 * 16; i ++) {
 
     let div = document.createElement('div');
-    div.className = "item";
+    div.className = "blocks";
     
     const mygrid = document.querySelector(".gamegrid");
     mygrid.appendChild(div);
+}
 
 
+
+const nodeList = document.querySelectorAll(".blocks");
+for (let i = 0; i < nodeList.length; i ++) {
+    nodeList[i].addEventListener("mouseover" , changeColor);
+}
+
+function changeColor(event) {
+    event.target.style.backgroundColor = "blue";
 }
